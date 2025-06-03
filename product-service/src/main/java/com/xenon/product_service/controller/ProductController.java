4d09e505e5272fc3032f370f1b7cf2b1b83a5257
Santino <santinob.dev@gmail.com>
2025-06-03 03:50:26 -0300

    feat: Implemented api-gateway. All conections working

--- product-service/src/main/java/com/xenon/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/xenon/product_service/controller/ProductController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/products")
+@RequestMapping("/api/products")
 @RequiredArgsConstructor
 @Tag(name = "Product Controller", description = "Operaciones CRUD para productos")
 public class ProductController {
